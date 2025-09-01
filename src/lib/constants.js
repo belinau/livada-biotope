a5640f02decd86e127cd90f6e86e7fba26931fa9
belinau <urban@bel.si>
2025-09-01 14:35:38 +0200

    Corrected sensor mapping.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,13 +1,10 @@
 export const BED_MAPPING = {
-    '!1641e779-0': { name: 'Tester Senzor J1', color: 'var(--primary)' },
-    '!1641e779-1': { name: 'Tester Senzor J2', color: 'var(--text-sage)' },
-    '!1641e779-2': { name: 'Tester Senzor J3', color: 'var(--primary-dark)' },
+    '!1641e779-0': { name: 'travnati sestoj', color: 'var(--primary)' },
+    '!1641e779-1': { name: 'sivka in melisa', color: 'var(--text-sage)' },
 
     '!04c5ad60-0': { name: 'barjansko rastje', color: 'var(--primary-dark)' },
     '!04c5ad60-1': { name: 'cvetlice za opraševalce', color: 'var(--border-color)' },
-    '!04c5ad60-2': { name: 'Senzor J3', color: 'var(--text-sage)' },
 
     '!76208ba5-0': { name: 'majaron in melisa', color: 'var(--text-sage)' },
     '!76208ba5-1': { name: 'tolščak in slezenovec', color: 'var(--primary-dark)' },
-    '!76208ba5-2': { name: 'Senzor J3', color: 'var(--border-color)' },
 };
