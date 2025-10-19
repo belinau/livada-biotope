2151ca94119bb4070a19d9819c8eb8d3d89ce805
belinau <urban@bel.si>
2025-10-19 16:47:16 +0200

    fixed regex issues.

--- src/pages/ContentItemPage.js
+++ src/pages/ContentItemPage.js
@@ -145,7 +145,7 @@ function ContentItemPage() {
 
     if (hasRenderError) {
         return (
-            <Page title={t('error')}>
+            <Page title={t('error')}> 
                 <Section title={t('error')}> 
                     <p className="text-center text-red-500">{t('contentRenderError')}</p>
                 </Section>
@@ -167,8 +167,8 @@ function ContentItemPage() {
     const createSummary = (markdown) => {
         if (!markdown) return '';
         const plainText = markdown
-            .replace(/!\\\[.*?\\].*?\\)/g, '') // remove images
-            .replace(/\\\\[(.*?)\\\]\(.*?\\)/g, '$1') // remove links, keeping text
+            .replace(/!\\[.*?\\]\(.*?\)/g, '') // remove images
+            .replace(/\\[(.*?)\\]\(.*?\)/g, '$1') // remove links, keeping text
             .replace(/\*\*|\*|_|---/g, '') // remove bold, italic, code, hr
             .replace(/#+\s/g, '') // remove headings
             .replace(/{{\s*.*?}}\n/g, ' ') // remove shortcodes and newlines
